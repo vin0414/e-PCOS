4bc0916a565e78f0a3aed202a1033662e269fb53
vin0414 <vinmogate@gmail.com>
2024-01-20 09:35:57 +0800

    add routes, controller

---
+++ app/Views/login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="assets/img/logo.png" rel="icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+    <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script> 
+    <script src= "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"> </script>
+</head>
+<body>
+  <div class="login-page">
+    <div class="img">
+    <img class=".img-fluid" src="assets/img/logo.png" alt="logo" style="width:100%;">
+    </div>
+    <div class="background-overlay"></div>
+      <div class="form">
+        <form class="register-form" method="POST">
+            <h2><i class="fas fa-lock"></i> Register</h2>
+            <input type="text" placeholder="Full Name *" required/>
+            <input type="text" placeholder="Username *" required/>
+            <input type="email" placeholder="Email *" required/>
+            <input type="password" placeholder="Password *" required/>
+            <button type="submit">create</button>
+            <p class="message">Already registered? <a href="javascript:void(0);" id="login-link">Sign In</a></p>
+            <a href="/">Go to Home</a>
+        </form>
+        <form class="login-form" method="post">
+            <h2><i class="fas fa-lock"></i> Login</h2>
+            <input type="text"placeholder="Username" required />
+            <input type="password" placeholder="Password" required/>
+            <button type="submit" name="send2">login</button>
+            <p class="message">Not registered? <a href="javascript:void(0);" id="register-link">Create an account</a></p>
+          <a href="/">Back to Home</a>
+       </form>
+      </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('#register-link').click(function() {
+         $('.login-form').hide();
+          $('.register-form').show();
+       });
+
+    $('#login-link').click(function() {
+      $('.register-form').hide();
+      $('.login-form').show();
+    });
+  });
+</script>
+</body>
+</html>
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -18,7 +18,7 @@
       <header class="header">
          <nav class="nav container">
             <div class="nav__data">
-               <a href="#" class="nav__logo">
+               <a href="/" class="nav__logo">
                   <img src="assets/img/logo.png" alt="logo">PCOS Awareness
                </a>
                
@@ -53,23 +53,11 @@
                               <i class="ri-service-line"></i> Food & Exercise
                            </a>
                         </li>
-
-                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-sticky-note-line"></i> Blog
-                           </a>
-                        </li>
-
-                          <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-bookmark-line"></i> Glossary
-                           </a>
-                        </li>
-
                      </ul>
                   </li>
-                  <li><a href="#" class="nav__link">Data Analytics</a></li>
-                  <li><a href="contacts" class="nav__link">Contact</a></li>
+                  <li><a href="/blogs" class="nav__link"> Blogs</a></li>
+                  <li><a href="/contacts" class="nav__link">Contact</a></li>
+                  <li><a href="/register" class="nav__link">Register</a></li>
             </div>
          </nav>
       </header>
@@ -81,7 +69,7 @@
       decision-making, and individualized wellness measures,<br>eventually fostering enhanced
       wellness results and,<br> a greater awareness of responsibility for their own health.</p>
 
-      <a href="login.html" class="btn">Take a Test!</a>
+      <a href="<?=site_url('/login')?>" class="btn">Take a Test!</a>
    </div>
 </section>
 
