91978a94a856ee7be69f5058a4cf34e07631cac2
vin0414 <vinmogate@gmail.com>
2024-01-21 11:10:56 +0800

    update template

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,43 +1,126 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <link href="assets/img/logo.png" rel="icon">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
-    <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script> 
-    <script src= "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"> </script>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PCOS Awareness</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <style>
+    input[type='text'],input[type='email'],input[type='password']{padding:10px;}
+  </style>
 </head>
+
 <body>
-  <div class="login-page">
-    <div class="background-overlay"></div>
-      <div class="form">
-        <?php if(!empty(session()->getFlashdata('fail'))) : ?>
-            <div class="alert alert-danger" role="alert">
-                <?= session()->getFlashdata('fail'); ?>
-            </div>
-        <?php endif; ?>
-        <form class="login-form" method="post" action="<?=site_url('sign-in')?>">
-            <center>
-                <img class=".img-fluid" src="assets/img/logo.png" alt="logo" width="100">
-            </center>
-            <br/>
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required/>
-            <button type="submit" name="send2">login</button>
-            <p class="message">Not registered? <a href="/register">Create an account</a>
-            <br/>Forgot Password? <a href="/forgot-password">Click here</a></p>
-            <a href="/">Go to Home</a>
-       </form>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex justify-content-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
+        <i class="bi bi-phone"></i> +1 5589 55488 55
       </div>
+      <div class="d-none d-lg-flex social-links align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <!-- <h1 class="logo me-auto"><a href="/">e-PCOS</a></h1> -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link" href="/">Home</a></li>
+          <li><a class="nav-link active" href="<?=site_url('login')?>">Sign-In</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main" style="margin-top:100px;">
+    <!-- ======= Contact Section ======= -->
+    <section class="why-us">
+      <div class="container">
+        <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <center>
+                  <img class=".img-fluid" src="assets/img/logo.png" alt="logo" width="100">
+              </center>
+              <?php if(!empty(session()->getFlashdata('fail'))) : ?>
+                  <div class="alert alert-danger" role="alert">
+                      <?= session()->getFlashdata('fail'); ?>
+                  </div>
+              <?php endif; ?>
+              <form method="POST" class="row mt-3" role="form" action="<?=base_url('sign-in')?>">
+                <div class="form-group mt-3">
+                  <input type="email" name="email" class="form-control" placeholder="Email Address" required/>
+                </div>
+                <div class="form-group mt-3">
+                  <input type="password" name="password" class="form-control" placeholder="Password" required/>
+                </div>
+                <div class="form-group mt-3">
+                  <button type="submit" class="form-control btn btn-primary">Sign-In</button>
+                </div>
+              </form>
+              <p><small>Not Registered? Create account <a href="<?=site_url('register')?>">here</a></small><br/>
+              <small>Forgot Password? Click <a href="<?=site_url('forgot-password')?>">here</a></small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4"></div>
+        </div>
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
+
 </html>
