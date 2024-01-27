55e85f76d845fa43d2c4856be8e67d5242edfeac
vin0414 <vinmogate@gmail.com>
2024-01-27 12:12:16 +0800

    updates

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -53,7 +53,7 @@
 
       <!-- <h1 class="logo me-auto"><a href="/">e-PCOS</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="<?=site_url('admin/dashboard')?>" class="logo me-auto"><img src="../assets/img/logo.png" alt="" class="img-fluid"></a>
+      <a href="<?=site_url('admin/dashboard')?>" class="logo me-auto"><img src="../assets/img/logo.png" alt="" class="img-fluid"> e-PCOS</a>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
